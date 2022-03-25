31fc7f601b865f3bd87b0ca514f20e364ac2a362
Iqbal_Arrafi <iqbalarrafi39@gmail.com>
2022-03-25 21:23:30 +0700

    Menambahkan logika aritmatika dan reset view

--- app/src/main/java/org/d3if4091/kalkulatoramoeba/MainActivity.kt
+++ app/src/main/java/org/d3if4091/kalkulatoramoeba/MainActivity.kt
@@ -2,10 +2,39 @@ package org.d3if4091.kalkulatoramoeba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import org.d3if4091.kalkulatoramoeba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnHitung.setOnClickListener{hitungAmoeba()}
+        binding.btnReset.setOnClickListener{reset()}
+    }
+
+    private fun hitungAmoeba(){
+        var jumlahAwalAmoeba = binding.jumlahAwalAmoebaInp.text.toString().toDouble()
+        var jumlahPembelahanAmoeba = binding.jumlahPembelahanAmoebaInp.text.toString().toDouble()
+        var rentangWaktu = binding.rentangWaktuInp.text.toString().toDouble()
+        var jangkaWaktu = binding.jangkaWaktuInp.text.toString().toDouble()
+
+        var jumlahWaktuPembelahan = jangkaWaktu /  rentangWaktu
+
+        var hasil = jumlahAwalAmoeba *  (Math.pow(jumlahPembelahanAmoeba, jumlahWaktuPembelahan))
+
+        binding.hasil.text = Math.round(hasil).toString()
+    }
+
+    private fun reset(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnHitung.setOnClickListener{hitungAmoeba()}
+        binding.btnReset.setOnClickListener{reset()}
     }
 }
